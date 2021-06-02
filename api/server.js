8daa2c9ae91d78271995584319513f84360ef7d1
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-06-02 15:23:24 -0400

    logger

--- api/server.js
+++ api/server.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const cors = require('cors')
-const usersRouter = require('./users/users-router')
+const {logger, validateUserId, validatePost, validateUser} = require('./middleware/middleware')
+
 
 const server = express();
 
-server.use(cors())
-
-// remember express by default cannot parse JSON in request bodies
 server.use(express.json())
 // global middlewares and the user's router need to be connected here
 
-
-server.use('/api/users', usersRouter)
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require your server and launch it
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('Server Running on http://localhost:4000');
+server.listen(4001, () => {
+  console.log('Server Running on http://localhost:4001');
 });
