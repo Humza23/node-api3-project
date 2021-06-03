59421897f98fe647e71f7c1913e27ae56bad5c44
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-06-03 14:15:09 -0400

    wip

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // require your server and launch it
+require('dotenv').config()
 
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 5000
-
 
 server.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
